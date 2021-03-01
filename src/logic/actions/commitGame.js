4453a6ea9b17777a38a292cc14883741f68c3500
bhagle <ben.hagle@gmail.com>
2021-03-01 13:06:17 -0500

    Prevent user from committing game if there are invalid cells

--- src/logic/actions/commitGame.js
+++ src/logic/actions/commitGame.js
@@ -1,10 +1,19 @@
 export default function commitGame(draft) {
     const lastState = draft.gameStates[draft.currentState];
-    draft.gameStates = [lastState];
-    for (let cell of draft.gameStates[0].cells) {
-        if (cell.value) {
-            cell.editable = false;
-        }
+
+    if(lastState.invalidCells.length > 0)
+    {
+        // Prevent user from committing game
+        // Grey out commitGame button
     }
-    draft.currentState = 0;
+    else
+    {
+        draft.gameStates = [lastState];
+        for (let cell of draft.gameStates[0].cells) {
+            if (cell.value) {
+                cell.editable = false;
+            }
+        }
+        draft.currentState = 0;
+    }
 }
